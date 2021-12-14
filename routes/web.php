b93e1d942b328ae454e140c9b122ae6e8bf504cb
munimrahman <munimrh@gmail.com>
2021-12-14 20:38:51 +0600

    final

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::view('/{path?}', 'welcome');
 Route::resource('contact', ContactController::class);
